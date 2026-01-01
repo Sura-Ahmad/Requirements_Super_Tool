6482598b4f52107239e0019b92178b64bcc0446f
Sura <soraahmed196@gmail.com>
2026-01-01 14:35:36 +0300

    db name change

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Text
 from src.database.db import Base
 
 class PreElicitationResponse(Base):
-    __tablename__ = "pre_elicitation_responses"
+    __tablename__ = "pre_elicitation_User_responses"
 
     id = Column(Integer, primary_key=True, index=True)
     domain = Column(String, index=True)
