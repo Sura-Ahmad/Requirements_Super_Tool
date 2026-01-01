e044bdebc0e224ddfd806feae272a16401412033
Sura <soraahmed196@gmail.com>
2026-01-01 13:29:53 +0300

    main.py comments added and specify corsmid

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,22 +1,23 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
+from fastapi import FastAPI                                     #import FastApi class from FatApi library for endpoint creation
+from fastapi.middleware.cors import CORSMiddleware              #allow frontend to interact with backend
 from src.api.domain_routes import router as domain_router
 from src.api.pre_elicitation_routes import router as pre_router
-from src.database.db import engine
-from src.database.models import Base
+from src.database.db import engine                              # to communicate with the database
+from src.database.models import Base                            # Base is the parent class for all the database models(db schema/tables)
 
 app = FastAPI(title="Requirements Super Tool")
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    CORSMiddleware,         #without it React cannot ineract with FastAPI backend
+    allow_origins=["http://localhost:5173"],    # * means all origins are allowed to interact with backend(react/browser/postman etc)
+    allow_credentials=True, # cookies/tokens 
+    allow_methods=["Get","Post"],    # all requsets(get/put/post...etc)
+    allow_headers=["Authorization", "Content-Type"],    # content-type/authorization...etc  
 )
 
-Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine) #create the database tables based on the defined models
 
-app.include_router(domain_router)
-app.include_router(pre_router)
+app.include_router(domain_router) # each end point in domain_router is active
+app.include_router(pre_router)    # end points ready to recieve requests
+
+# CORSMiddleware include the common between requests
