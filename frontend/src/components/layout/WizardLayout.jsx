e2c49a9e6ce10260c00807a0d152cbc2206b6afc
Sura <soraahmed196@gmail.com>
2026-01-04 14:09:12 +0300

    save changes

--- frontend/src/components/layout/WizardLayout.jsx
+++ frontend/src/components/layout/WizardLayout.jsx
@@ -3,6 +3,8 @@ import Stepper from "./Stepper";
 
 const WizardLayout = ({ children, currentStep }) => {
   return (
+    
+    
     <div style={{ maxWidth: 900, margin: "0 auto", padding: 40 }}>
       <Header />
       <Stepper currentStep={currentStep} />
